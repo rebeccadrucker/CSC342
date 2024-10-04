c23ca696ed7c41526f6c856c3d3abffed6f32b88
Rebecca Drucker <rebecca.drucker@furman.edu>
2024-10-04 08:39:53 -0400

    add www

--- app/app.py
+++ app/app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello, World!"
+
+@app.route("/csc342groups")
+def csc_342_groups():
+    groups = {1: {'Content Specialist': ['Julia Hogg'], 
+                  'Designer': ['Sage DeVore'], 
+                  'Programmer(s)': ['Miles Dame', 'Hanna King']},
+              2: {'Content Specialist': ['Brady Fleuette'], 
+                  'Designer': ['David Lash'], 
+                  'Programmer(s)': ['Kyler Bailey', 'Brodee Clontz']},
+              3: {'Content Specialist': ['AJ Leary'], 
+                  'Designer': ['Khalid Ismail'], 
+                  'Programmer(s)': ['Matt Keller', 'Moultrie Dangerfield']},
+              4: {'Content Specialist': ['Collin Riddle'], 
+                  'Designer': ['Case Riddle'], 
+                  'Programmer(s)': ['Ellie Johnson', 'Jack Roberts']},
+              5: {'Content Specialist': ['Olivia Longsworth'], 
+                  'Designer': ['Flynn Nisbet'], 
+                  'Programmer(s)': ['Mengsrun Nit']},
+              6: {'Content Specialist': ['Jack Patterson'],
+                  'Designer': ['Charlie Fink'],
+                  'Programmer(s)': ['Emirhan Gencer']}}
+    
+    return jsonify(groups)
